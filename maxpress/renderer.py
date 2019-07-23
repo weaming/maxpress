b8ad81a4d1871655f49b1cfced241b740e6ce945
weaming <garden.yuen@gmail.com>
2019-07-23 19:41:33 +0800

    rm comments

--- maxpress/renderer.py
+++ maxpress/renderer.py
@@ -36,47 +36,7 @@ class MDown(mistune.Markdown):
             else:
                 body += self.tok()
         rv = self.renderer.list_item(body, depth)
-        # print("-" * 100)
-        # print(rv)
-        # print("=" * 100)
         return rv
-
-    # def output_list_item(self):
-    #     body = self.renderer.placeholder()
-    #     depth = self.list_depth
-    #     assert depth >= 0
-    #     while True:
-    #         token = self.pop()
-    #         if not token:
-    #             break
-    #
-    #         if token["type"] == "list_item_end":
-    #             # do not break for outer loop
-    #             print(depth, self.list_depth)
-    #             if depth - 1 == self.list_depth:  # list_end called
-    #                 # 递归出栈
-    #                 break
-    #             # elif depth == self.list_depth and depth == 1:
-    #             #     break
-    #         else:
-    #             t = self.token["type"]
-    #             if t == "text":
-    #                 body += self.tok_text()
-    #             else:
-    #                 if t in newline_types:
-    #                     # self.tokens.append(token)
-    #                     break
-    #                 else:
-    #                     if t.endswith("_end"):
-    #                         pass
-    #                     else:
-    #                         body += self.tok()
-    #
-    #     rv = self.renderer.list_item(body, depth)
-    #     print("-" * 100)
-    #     print(rv)
-    #     print("=" * 100)
-    #     return rv
 
 
 class MRender(mistune.Renderer, PanguRendererMixin):
