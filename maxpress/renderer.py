220b0be26705edfbacaa7ae8f3bdfb0bcf3ecce3
weaming <garden.yuen@gmail.com>
2019-07-23 19:26:41 +0800

    success for multiple levels of list

--- maxpress/renderer.py
+++ maxpress/renderer.py
@@ -24,48 +24,59 @@ class MDown(mistune.Markdown):
         elif self.token["type"] == "list_end":
             self.list_depth -= 1
 
-        print("-" * self.list_depth, self.list_depth, rv)
+        # print("-" * self.list_depth, self.list_depth, rv)
         return rv
-
-    def output_list(self):
-        ordered = self.token["ordered"]
-        body = self.renderer.placeholder()
-        while True:
-            token = self.pop()
-            if not token or token["type"] == "list_end":
-                break
-            body += self.tok()
-        return self.renderer.list(body, ordered)
 
     def output_list_item(self):
         body = self.renderer.placeholder()
         depth = self.list_depth
-        assert depth >= 0
-        while True:
-            token = self.pop()
-            if not token:
-                break
-
-            if token["type"] == "list_item_end":
-                # do not break for outer loop
-                if depth - 1 == self.list_depth:
-                    # 递归出栈
-                    break
+        while self.pop()["type"] != "list_item_end":
+            if self.token["type"] == "text":
+                body += self.tok_text()
             else:
-                t = self.token["type"]
-                if t == "text":
-                    body += self.tok_text()
-                else:
-                    # print(t)
-                    if t.endswith("_end"):
-                        pass
-                    else:
-                        if t in newline_types:
-                            break
-                        else:
-                            body += self.tok()
+                body += self.tok()
+        rv = self.renderer.list_item(body, depth)
+        # print("-" * 100)
+        # print(rv)
+        # print("=" * 100)
+        return rv
 
-        return self.renderer.list_item(body)
+    # def output_list_item(self):
+    #     body = self.renderer.placeholder()
+    #     depth = self.list_depth
+    #     assert depth >= 0
+    #     while True:
+    #         token = self.pop()
+    #         if not token:
+    #             break
+    #
+    #         if token["type"] == "list_item_end":
+    #             # do not break for outer loop
+    #             print(depth, self.list_depth)
+    #             if depth - 1 == self.list_depth:  # list_end called
+    #                 # 递归出栈
+    #                 break
+    #             # elif depth == self.list_depth and depth == 1:
+    #             #     break
+    #         else:
+    #             t = self.token["type"]
+    #             if t == "text":
+    #                 body += self.tok_text()
+    #             else:
+    #                 if t in newline_types:
+    #                     # self.tokens.append(token)
+    #                     break
+    #                 else:
+    #                     if t.endswith("_end"):
+    #                         pass
+    #                     else:
+    #                         body += self.tok()
+    #
+    #     rv = self.renderer.list_item(body, depth)
+    #     print("-" * 100)
+    #     print(rv)
+    #     print("=" * 100)
+    #     return rv
 
 
 class MRender(mistune.Renderer, PanguRendererMixin):
@@ -83,6 +94,11 @@ class MRender(mistune.Renderer, PanguRendererMixin):
         else:
             rv = mistune.escape(text)
         return rv
+
+    def list_item(self, text, depth=None):
+        if depth is not None:
+            return '<li data-depth="%s">%s</li>\n' % (depth, text)
+        return "<li>%s</li>\n" % text
 
 
 markdown = MDown(renderer=MRender())
