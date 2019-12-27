fddd74d6d790eedfb2518b25a43d347c3792a0df
weaming <garden.yuen@gmail.com>
2019-12-27 14:50:27 +0800

    patch render_link to add target="_blank"

--- maxpress/renderer.py
+++ maxpress/renderer.py
@@ -1,3 +1,4 @@
+import os
 from mistletoe import Document
 
 from mistletoe_contrib.text_renderer import TextRenderer
@@ -5,9 +6,23 @@ from mistletoe_contrib.mathjax import MathJaxRenderer
 from mistletoe_contrib.pygments_renderer import PygmentsRenderer
 from mistletoe_contrib.toc_renderer import TOCRenderer
 
+HOSTNAME = os.getenv('HOSTNAME')
+
 
 class MixRender(TOCRenderer, PygmentsRenderer, MathJaxRenderer, TextRenderer):
-    pass
+    def render_link(self, token):
+        template = '<a href="{href}"{title}{target}>{inner}</a>'
+        href = self.escape_url(token.target)
+        if token.title:
+            title = ' title="{}"'.format(self.escape_html(token.title))
+        else:
+            title = ''
+        if HOSTNAME and HOSTNAME.lower() not in token.target:
+            target = ' target="_blank"'
+        else:
+            target = ''
+        inner = self.render_inner(token)
+        return template.format(href=href, title=title, inner=inner, target=target)
 
 
 def mistletoe_parse(text, toc=False):
